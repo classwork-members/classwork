f2cbb7cf36ef5886a7d1c8c34340f47f8d71e30b
jsliu622 <jsliu622@163.com>
2018-03-21 15:24:54 +0800

    login code

--- src/main/java/com/soft/classwork/model/User.java
+++ src/main/java/com/soft/classwork/model/User.java
@@ -1,9 +1,16 @@
 package com.soft.classwork.model;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User  {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+public class User implements UserDetails {
   private String userid;
   private String password;
   private String username;
@@ -44,48 +51,47 @@ public class User  {
     this.roles = roles;
   }
 
-//  @JsonIgnore
-//  @Override
-//  public Collection<? extends GrantedAuthority> getAuthorities() {
-//    List<GrantedAuthority> authorities = new ArrayList<>();
-//    for (Role role : this.getRoles()) {
-//      authorities.add(new SimpleGrantedAuthority(role.getRolename()));
-//    }
-//    return authorities;
-//  }
-//
-//  @JsonIgnore
-//  @Override
+  @JsonIgnore
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    for (Role role : this.getRoles()) {
+      authorities.add(new SimpleGrantedAuthority(role.getRolename()));
+    }
+    return authorities;
+  }
+
+  @JsonIgnore
+  @Override
   public String getPassword() {
     return this.password;
   }
 
-//  @Override
+  @Override
   public String getUsername() {
     return this.username;
   }
-//
-//  @JsonIgnore
-//  @Override
-//  public boolean isAccountNonExpired() {
-//    return true;
-//  }
-//
-//  @JsonIgnore
-//  @Override
-//  public boolean isAccountNonLocked() {
-//    return true;
-//  }
-//
-//  @JsonIgnore
-//  @Override
-//  public boolean isCredentialsNonExpired() {
-//    return true;
-//  }
-//
-//  @Override
-//  public boolean isEnabled() {
-//    return true;
-//  }
 
+  @JsonIgnore
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @JsonIgnore
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @JsonIgnore
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
