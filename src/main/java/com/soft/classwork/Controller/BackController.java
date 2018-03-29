001d63bd1f46a78239b0d7fcb25b18454d2ef116
sjl872964789 <872964789@qq.com>
2018-03-29 10:26:39 +0800

    add lxh

--- src/main/java/com/soft/classwork/Controller/BackController.java
+++ src/main/java/com/soft/classwork/Controller/BackController.java
@@ -1,6 +1,14 @@
 package com.soft.classwork.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.soft.classwork.service.CaseService;
+import com.soft.classwork.model.Result;
+import com.soft.classwork.utils.ResultUtil;
+import net.sf.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * Created by Yuukiiii
@@ -10,5 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackController {
 
+    @Autowired
+    CaseService caseService;
 
+    @PostMapping(value = "/hello")
+    public Object hello(@RequestParam("username") String username,
+                        @RequestParam("password") String password ){
+        Result result = new Result();
+        if(username.equals("123")&&password.equals("123")){
+            return ResultUtil.success("1");
+        }else
+            return null;
+    }
+
+    @PostMapping(value = "/manageCase2/{bingzhong}")
+    public Object test2(@PathVariable("bingzhong") String bingzhong){
+        return caseService.findCasesByIllnessid(8);
+    }
 }
--- src/main/java/com/soft/classwork/repository/DeptRepository.java
+++ src/main/java/com/soft/classwork/repository/DeptRepository.java
@@ -10,5 +10,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface DeptRepository extends JpaRepository<Department,Integer>{
-
 }
