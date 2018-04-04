3b3a1cd1843685317a0c4b4bea1495c8587aeca2
jsliu622 <jsliu622@163.com>
2018-04-04 14:40:38 +0800

    department

--- src/main/java/com/soft/classwork/service/DeptService.java
+++ src/main/java/com/soft/classwork/service/DeptService.java
@@ -1,5 +1,6 @@
 package com.soft.classwork.service;
 
+import com.soft.classwork.mapper.DeptMapper;
 import com.soft.classwork.model.Department;
 import com.soft.classwork.repository.DeptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,23 +18,23 @@ import java.util.List;
 public class DeptService {
 
     @Autowired
-    DeptRepository deptRepository ;
+    DeptMapper deptMapper ;
 
-    public Department addDept(Department department){
+    public int addDept(Department department){
 
-        return deptRepository.save(department);
+        return deptMapper.insertDept(department);
     }
 
-    public void deleteDept(Integer deptid){
-        deptRepository.deleteById(deptid);
+    /*public void deleteDept(Integer deptid){
+        deptMapper.deleteById(deptid);
     }
 
     public List<Department> findAllDept(){
-        return deptRepository.findAll();
+        return deptMapper.findAll();
     }
 
     public Department findOenDept(Integer deptid){
-        return deptRepository.getOne(deptid);
+        return deptMapper.getOne(deptid);
     }
 
     public Department updateDept(Integer deptid, String deptname, String deptdesc){
@@ -41,6 +42,6 @@ public class DeptService {
         department.setDeptdesc(deptdesc);
         department.setDeptid(deptid);
         department.setDeptname(deptname);
-        return deptRepository.save(department);
-    }
+        return deptMapper.save(department);
+    }*/
 }
