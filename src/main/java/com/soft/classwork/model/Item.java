cb06dee9e530848f21655d5a013e8b115f6e9553
jsliu622 <jsliu622@163.com>
2018-04-12 13:46:49 +0800

    user search join roles

---
+++ src/main/java/com/soft/classwork/model/Item.java
@@ -0,0 +1,22 @@
+package com.soft.classwork.model;
+
+public class Item {
+    private String item_name;
+    private int item_count;
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public int getItem_count() {
+        return item_count;
+    }
+
+    public void setItem_count(int item_count) {
+        this.item_count = item_count;
+    }
+}
