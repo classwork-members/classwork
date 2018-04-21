7ea51b20c826a4a0b394b2a33a5727c1f7ad1524
Yuukiiii <yuukiiiichan@gmail.com>
2018-04-21 14:07:25 +0800

    添加illness相关操作

--- src/main/java/com/soft/classwork/Controller/IllnessController.java
+++ src/main/java/com/soft/classwork/Controller/IllnessController.java
@@ -1,10 +1,33 @@
 package com.soft.classwork.Controller;
 
+import com.soft.classwork.enums.ResultEnum;
+import com.soft.classwork.model.Illness;
+import com.soft.classwork.model.Result;
+import com.soft.classwork.service.IllnessService;
+import com.soft.classwork.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Yuukiiii
  * on 2018/4/21 14:01
  */
 
-
+@RestController
+@RequestMapping(value = "/illness")
 public class IllnessController {
+
+    @Autowired
+    IllnessService illnessService;
+
+    @RequestMapping(value = "getOneIllness")
+    public Result getOneIllness(@RequestParam(value = "illnessid") Integer illnessid){
+        Illness illness = illnessService.getOneIllness(illnessid);
+        if (illness==null){
+            return ResultUtil.Error(ResultEnum.GETDARA_FAIL);
+        }
+        return ResultUtil.success(illness);
+    }
 }
--- src/main/java/com/soft/classwork/service/IllnessService.java
+++ src/main/java/com/soft/classwork/service/IllnessService.java
@@ -1,10 +1,22 @@
 package com.soft.classwork.service;
 
+import com.soft.classwork.model.Illness;
+import com.soft.classwork.repository.IllnessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Yuukiiii
  * on 2018/4/21 14:00
  */
 
-
+@Service
 public class IllnessService {
+
+    @Autowired
+    IllnessRepository illnessRepository;
+
+    public Illness getOneIllness(Integer illnessid){
+        return illnessRepository.getOne(illnessid);
+    }
 }
