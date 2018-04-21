21fa151801b60767c7ab9162748ff1baef21ef9d
Yuukiiii <yuukiiiichan@gmail.com>
2018-04-21 14:06:29 +0800

    maintest

---
+++ src/main/java/com/soft/classwork/Controller/IllnessController.java
@@ -0,0 +1,10 @@
+package com.soft.classwork.Controller;
+
+/**
+ * Created by Yuukiiii
+ * on 2018/4/21 14:01
+ */
+
+
+public class IllnessController {
+}
---
+++ src/main/java/com/soft/classwork/service/IllnessService.java
@@ -0,0 +1,10 @@
+package com.soft.classwork.service;
+
+/**
+ * Created by Yuukiiii
+ * on 2018/4/21 14:00
+ */
+
+
+public class IllnessService {
+}
